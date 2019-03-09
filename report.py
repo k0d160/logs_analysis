4215a9c7e76380130757009047385279504ca296
Jesus Ricardo Bernal <k0d160@protonmail.com>
2019-03-09 02:25:10 +0000

    Get views from db

---
+++ report.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.5
+
+import psycopg2
+
+
+DBNAME = 'news'
+
+
+def get_popular_articles():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select * from pop_articles")
+    pop_articles = c.fetchall()
+    db.close()
+    return pop_articles
+
+
+def get_popular_authors():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select * from popular_authors")
+    pop_authors = c.fetchall()
+    db.close()
+    return pop_authors
+
+
+def get_errors():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select * from errors")
+    errors = c.fetchall()
+    db.close()
+    return errors
+
+articles = get_popular_articles()
+authors = get_popular_authors()
+errors = get_errors()
+
+print("\nThe most popular articles of all time are:\n")
+print("{0[0]}\n{1[0]}\n{2[0]}".format(*articles))
+print("\nThe most popular authors of all time are:\n")
+print("{0[0]}\n{1[0]}\n{2[0]}\n{3[0]}".format(*authors))
+print("\nThe day on which more than 1% of request"
+      " lead to errors was:\n")
+print("{0[0]}\n".format(*errors))
